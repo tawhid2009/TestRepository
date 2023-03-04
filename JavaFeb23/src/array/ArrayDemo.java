41a8312b3c0af8fa0ee1127bc4d26f24a01bb2b7
tawhid2009 <86757373+tawhid2009@users.noreply.github.com>
2023-03-04 12:59:24 -0500

    Update ArrayDemo.java

--- JavaFeb23/src/array/ArrayDemo.java
+++ JavaFeb23/src/array/ArrayDemo.java
@@ -3,7 +3,7 @@ package array;
 public class ArrayDemo {
 
 	public static void main(String[] args) {
-		
+		//this is an one dymentational array
 		String[] myArray = {"Java", "Eclipse"};
 		for(int i = 0; i < myArray.length; i++) {
 			
